bbd1873e85d12c86a0b16362e7659b5b22873bca
Tdragon1539 <73697550+Tdragon1539@users.noreply.github.com>
2021-03-18 10:50:10 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,10 +1,44 @@
 //YOUR FIREBASE LINKS
+var firebaseConfig = {
+      apiKey: "AIzaSyArHFmKANKuXg9EOkSbLnCZlrVIlicSwl8",
+      authDomain: "kwitter-f8833.firebaseapp.com",
+      databaseURL: "https://kwitter-f8833-default-rtdb.firebaseio.com",
+      projectId: "kwitter-f8833",
+      storageBucket: "kwitter-f8833.appspot.com",
+      messagingSenderId: "147178747584",
+      appId: "1:147178747584:web:9ed48bda3e9c68011a1f25"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    user_name= localStorage.getItem("user_name");
+room_name= localStorage.getItem("room_name");
+
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+console.log(firebase_message_id);
+console.log(message_data);
+name_with_tag= "<h4>"+user+"<img class='user_tick' src='tick.png'> </h4>";
 //End code
       } });  }); }
 getData();
+
+
+function send(){
+      msg= document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+like: 0,
+Message:msg,
+user: user_name
+      });
+
+      document.getElementById("msg").innerHTML="";
+}
+
+function logout(){
+      localStorage.removeItem(user_name);
+      localStorage.removeItem(room_name);
+      window.location.replace("index.html");
+}
